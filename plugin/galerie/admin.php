8705cdc7d41da2b3ae876643effc427fb574a105
MaxenceCauderlier <mx.koder@gmail.com>
2022-08-01 22:15:16 +0200

    [FIX] Galerie hook
    BeforeSaveEditor

--- plugin/galerie/admin.php
+++ plugin/galerie/admin.php
@@ -29,7 +29,7 @@ switch($action){
 			$item = ($_REQUEST['id']) ?  $galerie->createItem($_REQUEST['id']) : new galerieItem();
 			$item->setCategory($_REQUEST['category']);
 			$item->setTitle($_REQUEST['title']);
-			$item->setContent($_REQUEST['content']);
+			$item->setContent($core->callHook('beforeSaveEditor',$_REQUEST['content']));
 			$item->setDate($_REQUEST['date']);
 			$item->setHidden((isset($_POST['hidden'])) ? 1 : 0);
 			if($galerie->saveItem($item)){
