a40e1323374a263a134afe6bb27f333e3fe8c426
MaxenceCauderlier <mx.koder@gmail.com>
2022-10-06 22:09:58 +0200

    Add ToogleSlide for menu mobile
    Add it to as a reusable function, and reduce time to fade In/Out

--- theme/default/scripts.js
+++ theme/default/scripts.js
@@ -9,53 +9,27 @@
  * @package 299Ko https://github.com/299Ko/299ko
  */
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     if (document.querySelector('#mobile_menu')) {
-        document.querySelector('#mobile_menu').addEventListener("click", function() {
-
+        document.querySelector('#mobile_menu').addEventListener("click", function () {
             var navigation = document.querySelector('#header #navigation');
-
-            if (!navigation.classList.contains('active')) {
-                // slideDown
-
-                navigation.classList.add('active');
-                navigation.style.height = "auto";
-
-                var height = navigation.clientHeight + "px";
-
-                navigation.style.height = "0px";
-
-                setTimeout(function() {
-                    navigation.style.height = height;
-                }, 0);
-
-            } else {
-                // slideUp
-
-                navigation.style.height = "0px";
-
-                navigation.addEventListener('transitionend', function() {
-                    navigation.classList.remove('active');
-                }, {once: true});
-            }
-
+            toggleSlide(navigation);
         });
     }
 
-
     var pathname = window.location.href.split('#')[0];
-    document.querySelectorAll('a[href^="#"]').forEach(function(item) {
+    document.querySelectorAll('a[href^="#"]').forEach(function (item) {
         var link = item.getAttribute('href');
         item.setAttribute('href', pathname + link);
     });
 
 
-    document.querySelectorAll('.msg').forEach(function(item, index) {
-        item.querySelector('.msg-button-close').addEventListener('click', function() {
+    document.querySelectorAll('.msg').forEach(function (item, index) {
+        item.querySelector('.msg-button-close').addEventListener('click', function () {
             fadeOut(item);
         });
 
-        setTimeout(function() {
+        setTimeout(function () {
             fadeOut(item);
         }, 5000 + index * 5000);
 
@@ -63,17 +37,39 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
+function toggleSlide(el) {
+    if (el.clientHeight > 0) {
+        el.classList.add('active');
+    }
+    if (!el.classList.contains('active')) {
+        // slideDown
+        el.classList.add('active');
+        el.style.height = "auto";
+        var height = el.clientHeight + "px";
+        el.style.height = "0px";
+        setTimeout(function () {
+            el.style.height = height;
+        }, 0);
+    } else {
+        // slideUp
+        el.style.height = "0px";
+        el.addEventListener('transitionend', function () {
+            el.classList.remove('active');
+        }, {once: true});
+    }
+}
 
 function fadeOut(el) {
     el.style.opacity = 1;
     (function fade() {
-        if ((el.style.opacity -= .1) < 0) {
+        if ((el.style.opacity -= .03) < 0) {
             el.style.display = "none";
         } else {
             requestAnimationFrame(fade);
         }
     })();
-};
+}
+;
 function fadeIn(el, display) {
     el.style.opacity = 0;
     el.style.display = display || "block";
