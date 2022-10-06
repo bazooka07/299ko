808b22e5775791a45cb330cf405dddca299bd613
shevabam <shevabam@gmail.com>
2022-10-06 14:51:42 +0200

    JS Vanilla instead of jQuery for galerie plugin (admin)

--- plugin/galerie/template/admin.js
+++ plugin/galerie/template/admin.js
@@ -1,10 +1,33 @@
-$(document).ready(function () {
-    $(".galerie-admin tr.hidden").hide();
-    $(".galerie-admin .showall").click(function () {
-        $(".galerie-admin tr.visible").toggle();
-        $(".galerie-admin tr.hidden").toggle();
-    });
-    $(".galerie-admin .category").click(function () {
-        $(".galerie-admin #category").val($(this).text());
-    });
+document.addEventListener("DOMContentLoaded", function() {
+
+    if (document.querySelector('.galerie-admin table')) {
+
+        document.querySelector('.galerie-admin tr.hidden').style.display = 'none';
+
+        document.querySelector('.galerie-admin .showall').addEventListener('click', function(){
+            document.querySelectorAll('tr.visible').forEach(function(item, index){
+                if (item.style.display != 'none') {
+                    item.style.display = 'none';
+                } else {
+                    item.style.display = 'block';
+                }
+            });
+
+            document.querySelectorAll('tr.hidden').forEach(function(item, index){
+                if (item.style.display != 'none') {
+                    item.style.display = 'none';
+                } else {
+                    item.style.display = 'block';
+                }
+            });
+        });
+
+    }
+
+    if (document.querySelector('.galerie-admin .category')) {
+        document.querySelector('.galerie-admin .category').addEventListener('click', function(){
+            document.querySelector('.galerie-admin input#category').value = this.textContent;
+        });
+    }
+
 });
