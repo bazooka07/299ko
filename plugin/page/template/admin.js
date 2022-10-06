064652b9a8e5f39ba879770395b504586c51e8fa
shevabam <shevabam@gmail.com>
2022-10-06 14:38:35 +0200

    JS Vanilla instead of jQuery for page plugin (admin)

--- plugin/page/template/admin.js
+++ plugin/page/template/admin.js
@@ -1,4 +1,10 @@
-$(document).ready(function () {
-    $(".page-admin tr:first-child .up").hide();
-    $(".page-admin tr:last-child .down").hide();
+document.addEventListener("DOMContentLoaded", function() {
+
+    if (document.querySelector('.page-admin table')) {
+
+        document.querySelector('.page-admin tr:first-child .up').style.display = 'none';
+        document.querySelector('.page-admin tr:last-child .down').style.display = 'none';
+
+    }
+
 });
