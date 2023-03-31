fa3f22148ae87ffa48da6cb3bdf062e446819506
Max Koder <mx.koder@gmail.com>
2023-03-31 15:03:40 +0200

    [FIX] Seo script when ID is empty
    Dont echo script if analytics ID are empty

--- plugin/seo/seo.php
+++ plugin/seo/seo.php
@@ -22,6 +22,9 @@ function seoInstall() {
 
 function seoEndFrontHead() {
     $plugin = pluginsManager::getInstance()->getPlugin('seo');
+    if ($plugin->getConfigVal('trackingId') === false || $plugin->getConfigVal('trackingId') === '') {
+        return;
+    }
     $temp = "<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
