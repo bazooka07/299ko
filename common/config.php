962fbba78bc5e3ac67a1b19d93df7778dc5e5ce9
Max Koder <mx.koder@gmail.com>
2025-03-02 17:37:36 +0100

    MAJ to F-A 6.7.2

--- common/config.php
+++ common/config.php
@@ -20,7 +20,7 @@
  const THEMES = ROOT . 'theme/';
  const PLUGINS = ROOT . 'plugin/';
  const ADMIN_PATH = ROOT . 'admin/';
- const FONTICON = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css';
+ const FONTICON = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css';
  const FANCYCSS = 'https://cdnjs.cloudflare.com/ajax/libs/fancyapps-ui/4.0.31/fancybox.min.css';
  const FANCYJS = 'https://cdnjs.cloudflare.com/ajax/libs/fancyapps-ui/4.0.31/fancybox.umd.min.js';
  
